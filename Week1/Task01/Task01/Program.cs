14fef2a23c411a571aef38f05facd68995fb4d69
AlmazPP2 <almaz.takibayev@gmail.com>
2019-01-28 01:39:39 +0600

    Week 1

---
+++ Week1/Task01/Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static bool IsPrime(int n)
+        {
+            if(n==2 || n == 3)
+            {
+                return true;
+            }
+            if(n%2==0 || n%3==0 || n == 1)
+            {
+                return false;
+            }
+
+            for(int i=2; i*i<=n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n= 
+        }
+    }
+}
---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            string s = Console.ReadLine();
+            string[] b = s.Split();
+            for (int i = 0; i < b.Length; i++)
+            {
+                arr[i] = int.Parse(b[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 2; j < arr[i]; j++)
+                {
+                    if (arr[i] % j == 0)
+                    {
+                        sum++;
+                    }
+                }
+                if (sum == 0 && arr[i] > 1)
+                {
+                    list.Add(arr[i]);
+                }
+            }
+
+            Console.WriteLine(list.Count);
+            for (int a = 0; a < list.Count; a++)
+            {
+                Console.Write(list[a] + " ");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
