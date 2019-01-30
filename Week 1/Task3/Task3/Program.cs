2d65678fdd86a4b5ea051fe8fd05298c9295dd60
AlmazPP2 <almaz.takibayev@gmail.com>
2019-01-30 15:08:29 +0600

    Task3

--- Week 1/Task3/Task3/Program.cs
+++ Week 1/Task3/Task3/Program.cs
@@ -6,16 +6,34 @@ using System.Threading.Tasks;
 
 namespace Task3
 {
-    class arr
-    {
-
-    }
-
-
     class Program
     {
+        static void Print(int n)
+        {
+            for(int i=0; i<2; i++)
+            {
+                Console.Write(n + " ");
+            }
+        }
+
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            //cоздаю стринг чтобы вводить в консоле, потом создаю массив стринга и добавляю в него начальный,стринг разделенный пробелами.
+            string s = Console.ReadLine();
+            string[] b = s.Split();
+            //создаю цикл чтобы добавить в массив чисел все числа которые я вводил в виде стринга в интовом виде
+            for (int i = 0; i < b.Length; i++)
+            {
+                arr[i] = int.Parse(b[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+                {
+                    Print(arr[i]);
+                }
+                Console.ReadKey();
         }
     }
 }
