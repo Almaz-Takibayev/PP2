7700754262ea1c8b05de0d5b513525b56608f384
AlmazPP2 <almaz.takibayev@gmail.com>
2019-02-08 23:02:48 +0600

    Task 2

--- PP2/Week 2/Task 2/Task 2/Program.cs
+++ PP2/Week 2/Task 2/Task 2/Program.cs
@@ -11,22 +11,26 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            //создаю массив простых чисел
             int[] PrimeNumbers;
 
+            //считываю данные из файла
             using (FileStream fs = new FileStream(@"C:\Week2\Input.txt", FileMode.Open, FileAccess.Read))
             {
                 using (StreamReader sr = new StreamReader(fs))
                 {
+                    //фильтр чисел
                     string text = sr.ReadLine();
                     int[] numbers = Numbers(text);
                     PrimeNumbers = IsPrime(numbers);
                 }
             }
-
+            //записали результат на другой файл
             using (FileStream fs2 = new FileStream(@"C:\Week2\Output", FileMode.OpenOrCreate, FileAccess.Write))
             {
                 using (StreamWriter sw = new StreamWriter(fs2))
                 {
+                    //запись простых чисел через пробел
                     foreach (var x in PrimeNumbers)
                     {
                         sw.Write(x + " ");
@@ -36,7 +40,11 @@ namespace Task_2
         }
 
 
-
+        /// <summary>
+        /// проверка на простые числа
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         private static int[] IsPrime(int[] num)
         {
             List<int> res = new List<int>();
@@ -59,6 +67,11 @@ namespace Task_2
             return res.ToArray();
         }
 
+        /// <summary>
+        /// превращение входных данных в массив чисел
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         private static int[] Numbers(string text)
         {
             string[] parts = text.Split();
